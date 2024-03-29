54b4939b09395be4f9759f8b338f5f2e980a0d2e
bstawy <125200508@sha.edu.eg>
2024-03-29 02:37:23 +0200

    Add DocDocApp widget to lib/doc_doc_app.dart
    Create a new DocDocApp widget in lib/doc_doc_app.dart file to serve as the root of the application.

---
+++ lib/doc_doc_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DocDocApp extends StatelessWidget {
+  const DocDocApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'DocDoc',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
