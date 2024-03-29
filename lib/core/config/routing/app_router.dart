6c6c19b27ba570a14015ec76f5bc20926973ee21
bstawy <125200508@sha.edu.eg>
2024-03-29 19:00:29 +0200

    Add AppRouter for handling navigation routes
    Implement AppRouter class to handle navigation routes in the application.
    Includes handling routes for onboarding, login, register screens, and a default case for undefined routes.

---
+++ lib/core/config/routing/app_router.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import '../../../features/login/ui/login_screen.dart';
+import '../../../features/on_boarding/on_boarding_screen.dart';
+import '../../../features/register/ui/register_screen.dart';
+import 'routes.dart';
+
+class AppRouter {
+  Route generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(
+          builder: (_) => const OnBoardingScreen(),
+        );
+
+      case Routes.login:
+        return MaterialPageRoute(
+          builder: (_) => const LoginScreen(),
+        );
+
+      case Routes.register:
+        return MaterialPageRoute(
+          builder: (_) => const RegisterScreen(),
+        );
+
+      default:
+        return MaterialPageRoute(
+          builder: (_) => NoRouteDefinedWidget(settings: settings),
+        );
+    }
+  }
+}
+
+class NoRouteDefinedWidget extends StatelessWidget {
+  final RouteSettings settings;
+  const NoRouteDefinedWidget({super.key, required this.settings});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "No Route Defined for ${settings.name}",
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
