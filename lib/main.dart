af57d4a60a357b2e16e4513468db6c6b087d1d44
bstawy <125200508@sha.edu.eg>
2024-03-30 15:21:44 +0200

    Add GlobalKey for NavigatorState
    This will be used for managing navigation within the app.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'core/config/routing/app_router.dart';
 import 'doc_doc_app.dart';
 
+GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
 void main() {
   runApp(DocDocApp(appRouter: AppRouter()));
 }
