da7c74a8eb489c8bffeccd1bc0d415cf4e50e742
bstawy <125200508@sha.edu.eg>
2024-03-30 15:22:05 +0200

    Add AppConstants file with theme and mediaQuery variables

---
+++ lib/core/config/Constants/app_constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+import '../../../main.dart';
+
+class AppConstants {
+  static var theme = Theme.of(navigatorKey.currentState!.context);
+  static var mediaQuery = MediaQuery.of(navigatorKey.currentState!.context);
+}
