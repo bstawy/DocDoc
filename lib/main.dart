9d99285884d93ca72906a3414c3637f520682236
bstawy <125200508@sha.edu.eg>
2024-03-31 16:14:11 +0200

    Remove unnecessary GlobalKey declaration

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import 'core/config/routing/app_router.dart';
 import 'doc_doc_app.dart';
 
-GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-
 void main() {
   runApp(DocDocApp(appRouter: AppRouter()));
 }
