0b47c72aebd25ac8c27c3d9ab54d375b8331e3c6
bstawy <125200508@sha.edu.eg>
2024-04-01 20:18:41 +0200

    Replaced GetStartedButton with CustomMaterialButton for better functionality and consistency.

--- lib/features/on_boarding/on_boarding_screen.dart
+++ lib/features/on_boarding/on_boarding_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../core/config/routing/routes.dart';
 import '../../core/config/theme/texts/text_styles.dart';
 import '../../core/helpers/extensions/extensions.dart';
+import '../../core/widgets/custom_material_button.dart';
 import 'widgets/doctor_image_and_text.dart';
-import 'widgets/get_started_button.dart';
 import 'widgets/header_logo_and_name.dart';
 
 class OnBoardingScreen extends StatelessWidget {
@@ -28,7 +29,12 @@ class OnBoardingScreen extends StatelessWidget {
                     style: TextStyles.font12GreyRegular,
                   ),
                   verticalSpace(32.h),
-                  const GetStartedButton(),
+                  CustomMaterialButton(
+                    onClicked: () {
+                      context.pushNamed(Routes.loginScreen);
+                    },
+                    title: "Get Started",
+                  ),
                 ],
               ).setHorizontalPadding(32.w),
             ],
