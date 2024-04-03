45f0afdfbd17d47c422b313441f71ece981cf64e
bstawy <125200508@sha.edu.eg>
2024-04-03 21:48:09 +0200

    Update login screen UI components
    - Update WelcomeHeader widget with new title and description
    - Replace AlreadyHaveAnAccount widget with DontHaveAnAccount widget

--- lib/features/auth/login/ui/login_screen.dart
+++ lib/features/auth/login/ui/login_screen.dart
@@ -4,11 +4,11 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../core/di/dependency_injection.dart';
 import '../../../../core/helpers/extensions/extensions.dart';
+import '../../widgets/header.dart';
+import '../../widgets/terms_and_conditions_text.dart';
 import '../logic/login_cubit.dart';
-import 'widgets/already_have_an_account.dart';
+import 'widgets/dont_have_an_account.dart';
 import 'widgets/login_form.dart';
-import 'widgets/terms_and_conditions_text.dart';
-import 'widgets/welcome_header.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -21,7 +21,11 @@ class LoginScreen extends StatelessWidget {
           child: Column(
             children: [
               verticalSpace(32.h),
-              const WelcomeHeader(),
+              const WelcomeHeader(
+                title: "Welcome Back",
+                description:
+                    "We're excited to have you back, can't wait to see what you've been up to since you last logged in.",
+              ),
               verticalSpace(32.h),
               BlocProvider(
                 create: (context) => getIt<LoginCubit>(),
@@ -30,7 +34,7 @@ class LoginScreen extends StatelessWidget {
               verticalSpace(32.h),
               const TermsAndConditionsText(),
               verticalSpace(24.h),
-              const AlreadyHaveAnAccount(),
+              const DontHaveAnAccount(),
             ],
           ).setVerticalPadding(32.h),
         ),
