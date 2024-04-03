92748e1ff3053803b4dcab762e1a4bdc18ce4ec7
bstawy <125200508@sha.edu.eg>
2024-04-03 17:27:34 +0200

    Fix imports

--- lib/core/networking/api_result/api_result.dart
+++ lib/core/networking/api_result/api_result.dart
@@ -1,5 +1,6 @@
-import 'package:docdoc/core/networking/api_error/api_error_handler.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../api_error/api_error_handler.dart';
 
 part 'api_result.freezed.dart';
 
