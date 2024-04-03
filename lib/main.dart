85b0461c75ef2d069fd5290336718e73907cc26d
bstawy <125200508@sha.edu.eg>
2024-04-03 04:03:49 +0200

    Initialize get it for di

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
 import 'core/config/routing/app_router.dart';
+import 'core/di/dependency_injection.dart';
 import 'doc_doc_app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  initGetIt();
   runApp(DocDocApp(appRouter: AppRouter()));
 }
