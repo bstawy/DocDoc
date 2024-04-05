48ea0d5c592d2e98f995e96343c6a2aaa5fd0ccf
bstawy <125200508@sha.edu.eg>
2024-04-05 20:43:09 +0200

    Refactor DocDocApp dev flavor initialization to include isDevFlavor flag and update main_development.dart

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'core/config/routing/app_router.dart';
 import 'core/di/dependency_injection.dart';
@@ -9,20 +10,16 @@ import 'doc_doc_app.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   initGetIt();
+
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Color(0xFF4CAF50),
+      systemNavigationBarColor: Color(0xFF4CAF50),
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ),
+  );
   runApp(DocDocApp(
     isDevFlavor: true,
     appRouter: AppRouter(),
   ));
 }
-
-/*
-Directionality(
-      textDirection: TextDirection.ltr,
-      child: Banner(
-          message: "    dev",
-          location: BannerLocation.topEnd,
-          color: Colors.green,
-          layoutDirection: TextDirection.ltr,
-          child: DocDocApp(appRouter: AppRouter())),
-    ),
-*/
