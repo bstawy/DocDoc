4e58dd8de0eb691f82a9ad05743a2f5192476e41
bstawy <125200508@sha.edu.eg>
2024-04-17 01:22:05 +0200

    Add InboxScreen widget to inbox_screen.dart

---
+++ lib/features/pages/chat/ui/inbox_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InboxScreen extends StatelessWidget {
+  const InboxScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Inbox Screen',
+        style: TextStyle(fontSize: 24, color: Colors.black),
+      ),
+    );
+  }
+}
