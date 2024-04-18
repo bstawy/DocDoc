639e21014166afbe2cb92274681e26d7ff3e6a94
bstawy <125200508@sha.edu.eg>
2024-04-19 00:24:59 +0200

    Update radius of CircleAvatar in BottomNavIconWidget

--- lib/features/layout/ui/widgets/bottom_nav_bar_icon.dart
+++ lib/features/layout/ui/widgets/bottom_nav_bar_icon.dart
@@ -55,7 +55,7 @@ class BottomNavIconWidget extends StatelessWidget {
             : Colors.transparent,
       ),
       child: CircleAvatar(
-        radius: 20.r,
+        radius: 15.r,
         backgroundColor: ColorsManager.white,
         child: const Icon(Icons.person),
       ),
