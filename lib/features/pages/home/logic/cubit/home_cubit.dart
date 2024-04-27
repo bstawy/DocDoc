c97d9f37b5c84ca43ab6bb8d97f927f6e2cd9227
bstawy <125200508@sha.edu.eg>
2024-04-28 01:16:02 +0300

    Add HomeState class for managing home screen state in home_cubit.dart

---
+++ lib/features/pages/home/logic/cubit/home_cubit.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../data/repos/home_repo.dart';
+import 'home_state.dart';
+
+class HomeCubit extends Cubit<HomeState> {
+  final HomeRepo _homeRepo;
+
+  HomeCubit(this._homeRepo) : super(const HomeState.initial());
+
+  void getHomeData() async {
+    emit(const HomeState.loading());
+    final response = await _homeRepo.getHomeData();
+
+    response.when(
+      success: (homeResponse) {
+        debugPrint("Home Response: $homeResponse");
+        emit(HomeState.success(homeResponse));
+      },
+      failure: (error) {
+        debugPrint("Home Error: ${error.apiErrorModel.message}");
+
+        emit(
+          HomeState.failure(error: error.apiErrorModel.message ?? ""),
+        );
+      },
+    );
+  }
+}
