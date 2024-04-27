af9458890b44081de755c18f82727c7e11058e88
bstawy <125200508@sha.edu.eg>
2024-04-28 01:16:28 +0300

    Refactor HomeScreen to use Flutter Bloc for managing state in home_screen.dart

--- lib/features/pages/home/ui/home_screen.dart
+++ lib/features/pages/home/ui/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../core/helpers/extensions/extensions.dart';
+import '../logic/cubit/home_cubit.dart';
+import '../logic/cubit/home_state.dart';
 import 'widgets/doctor_speciality_widget.dart';
 import 'widgets/header_widget.dart';
 import 'widgets/nearby_doctor_widget.dart';
@@ -12,22 +15,27 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      scrollDirection: Axis.vertical,
-      slivers: [
-        SliverList(
-          delegate: SliverChildListDelegate(
-            [
-              const Header(),
-              const NearbyDoctor(),
-              verticalSpace(24.h),
-              const DoctorSpeciality(),
-              verticalSpace(24.h),
-              const RecommendedDoctor(),
-            ],
-          ),
-        ),
-      ],
+    return BlocBuilder<HomeCubit, HomeState>(
+      bloc: context.read<HomeCubit>()..getHomeData(),
+      builder: (context, state) {
+        return CustomScrollView(
+          scrollDirection: Axis.vertical,
+          slivers: [
+            SliverList(
+              delegate: SliverChildListDelegate(
+                [
+                  const Header(),
+                  const NearbyDoctor(),
+                  verticalSpace(24.h),
+                  const DoctorSpeciality(),
+                  verticalSpace(24.h),
+                  const RecommendedDoctor(),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
     ).setHorizontalPadding(16.w);
   }
 }
