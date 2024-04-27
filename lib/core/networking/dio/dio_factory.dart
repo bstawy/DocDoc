39036c4a8ec5a9c010de570686f28366514eab2f
bstawy <125200508@sha.edu.eg>
2024-04-28 01:11:30 +0300

    Refactor DioFactory class to use separate Dio instances for different purposes

--- lib/core/networking/dio/dio_factory.dart
+++ lib/core/networking/dio/dio_factory.dart
@@ -1,35 +1,77 @@
 import 'package:dio/dio.dart';
+import 'package:docdoc/core/networking/dio/token_interceptor.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class DioFactory {
   // Create Singletone
   DioFactory._();
 
-  static Dio? dio;
+  static Dio? freeDio;
 
-  static Dio getDio() {
+  static Dio getFreeDio() {
     Duration timeOut = const Duration(seconds: 30);
 
-    if (dio == null) {
-      dio = Dio();
-      dio!
+    if (freeDio == null) {
+      freeDio = Dio();
+      freeDio!
         ..options.connectTimeout = timeOut
         ..options.receiveTimeout = timeOut;
-      addDioInterceptors();
-      return dio!;
+      addFreeDioInterceptors();
+      return freeDio!;
     } else {
-      return dio!;
+      return freeDio!;
     }
   }
 
-  static void addDioInterceptors() {
-    // TODO: add interceptor for authorization
-    dio?.interceptors.add(
-      PrettyDioLogger(
-        requestBody: true,
-        requestHeader: true,
-        responseHeader: true,
-      ),
+  static void addFreeDioInterceptors() {
+    freeDio?.interceptors.addAll(
+      [
+        TokenInterceptor(),
+        PrettyDioLogger(
+          requestBody: true,
+          requestHeader: true,
+          responseHeader: true,
+        ),
+      ],
     );
   }
+
+  // static Dio? tokenDio;
+
+  // static Dio getTokenDio() {
+  //   Duration timeOut = const Duration(seconds: 30);
+
+  //   if (tokenDio == null) {
+  //     tokenDio = Dio();
+  //     tokenDio!
+  //       ..options.connectTimeout = timeOut
+  //       ..options.receiveTimeout = timeOut;
+  //     addTokenDioInterceptors();
+  //     return tokenDio!;
+  //   } else {
+  //     return tokenDio!;
+  //   }
+  // }
+
+  // static void addTokenDioInterceptors() {
+  //   tokenDio?.interceptors.addAll(
+  //     [
+  //       PrettyDioLogger(
+  //         requestBody: true,
+  //         requestHeader: true,
+  //         responseHeader: true,
+  //       ),
+  //       InterceptorsWrapper(
+  //         onRequest: (options, handler) async {
+  //           String? mytoken =
+  //               await SecureStorage.getInstance().read(key: "mytoken");
+  //           options.headers['Authorization'] = "Bearer${mytoken!}";
+  //         },
+  //         onError: (DioException e, handler) {
+  //           log(e.message!);
+  //         },
+  //       ),
+  //     ],
+  //   );
+  // }
 }
