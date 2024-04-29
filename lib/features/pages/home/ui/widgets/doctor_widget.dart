39bf508f40a6cca1945cc3c03930e0a42e4580a4
bstawy <125200508@sha.edu.eg>
2024-04-29 23:51:07 +0300

    Add DoctorWidget to display doctor information in home page

---
+++ lib/features/pages/home/ui/widgets/doctor_widget.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../../core/config/theme/colors/light_color_scheme.dart';
+import '../../../../../core/config/theme/texts/text_styles.dart';
+import '../../../../../core/helpers/extensions/extensions.dart';
+import '../../data/models/doctor_model.dart';
+
+class DoctorWidget extends StatelessWidget {
+  final DoctorModel doctor;
+
+  const DoctorWidget({
+    super.key,
+    required this.doctor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 110.w,
+          height: 110.h,
+          decoration: BoxDecoration(
+            color: ColorsManager.lighterGrey,
+            borderRadius: BorderRadius.circular(12.r),
+          ),
+        ),
+        horizontalSpace(16.w),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              doctor.name,
+              style: TextStyles.font14DarkBlueMedium,
+            ),
+            verticalSpace(4.h),
+            Text(
+              doctor.specialization.name,
+              style: TextStyles.font12DarkBlueMedium,
+            ),
+            verticalSpace(4.h),
+            Text(
+              doctor.degree,
+              style: TextStyles.font12DarkBlueMedium,
+            ),
+            verticalSpace(4.h),
+            Text(
+              "${doctor.price} EGP",
+              style: TextStyles.font12DarkBlueMedium,
+            ),
+          ],
+        ),
+      ],
+    ).setOnlyPadding(0, 16.h, 0, 0);
+  }
+}
