79b365465c14768b1b30ac4885aa5c291d8d5189
bstawy <125200508@sha.edu.eg>
2024-04-29 22:46:18 +0300

    Refactor HomeScreen to remove unused imports and update widget structure

--- lib/features/pages/home/ui/home_screen.dart
+++ lib/features/pages/home/ui/home_screen.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../core/helpers/extensions/extensions.dart';
-import '../logic/home_cubit.dart';
-import '../logic/home_state.dart';
 import 'widgets/doctor_speciality_widget.dart';
 import 'widgets/header_widget.dart';
 import 'widgets/nearby_doctor_widget.dart';
@@ -15,31 +12,22 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final HomeCubit homeCubit = context.read<HomeCubit>();
-
-    return BlocListener<HomeCubit, HomeState>(
-      bloc: homeCubit..getHomeData(),
-      listener: (context, state) {},
-      child: CustomScrollView(
-        scrollDirection: Axis.vertical,
-        slivers: [
-          SliverList(
-            delegate: SliverChildListDelegate(
-              [
-                const Header(),
-                const NearbyDoctor(),
-                verticalSpace(24.h),
-                BlocProvider.value(
-                  value: homeCubit,
-                  child: const DoctorSpeciality(),
-                ),
-                verticalSpace(24.h),
-                const RecommendedDoctor(),
-              ],
-            ),
+    return CustomScrollView(
+      scrollDirection: Axis.vertical,
+      slivers: [
+        SliverList(
+          delegate: SliverChildListDelegate(
+            [
+              const Header(),
+              const NearbyDoctor(),
+              verticalSpace(24.h),
+              const DoctorSpeciality(),
+              verticalSpace(24.h),
+              const RecommendedDoctor(),
+            ],
           ),
-        ],
-      ).setHorizontalPadding(16.w),
-    );
+        ),
+      ],
+    ).setHorizontalPadding(16.w);
   }
 }
