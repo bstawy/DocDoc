3af4854b3b3960312303adeadb78e818e0a99554
bstawy <125200508@sha.edu.eg>
2024-04-29 22:03:37 +0300

    Refactor DoctorSpeciality widget to use BlocBuilder for dynamic rendering of specialist widgets

--- lib/features/pages/home/ui/widgets/doctor_speciality_widget.dart
+++ lib/features/pages/home/ui/widgets/doctor_speciality_widget.dart
@@ -1,15 +1,20 @@
+import 'package:docdoc/features/pages/home/logic/home_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../../../../../core/config/theme/colors/light_color_scheme.dart';
 import '../../../../../core/config/theme/texts/text_styles.dart';
 import '../../../../../core/helpers/extensions/extensions.dart';
+import '../../data/models/home_response_model.dart';
+import '../../logic/home_state.dart';
+import 'specialist_widget.dart';
 
 class DoctorSpeciality extends StatelessWidget {
   const DoctorSpeciality({super.key});
 
   @override
   Widget build(BuildContext context) {
+    List<HomeData> specialities = [];
     return Column(
       children: [
         Row(
@@ -30,33 +35,33 @@ class DoctorSpeciality extends StatelessWidget {
             ),
           ],
         ),
-        SizedBox(
-          width: double.maxFinite,
-          height: 130.h,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            padding: EdgeInsets.only(top: 16.h),
-            itemCount: 5,
-            itemBuilder: (context, index) {
-              return Column(
-                children: [
-                  CircleAvatar(
-                    radius: 40.r,
-                    backgroundColor: ColorsManager.lighterGrey,
-                    child: const Icon(
-                      Icons.medical_services,
-                      color: ColorsManager.darkBlue,
-                    ),
-                  ),
-                  verticalSpace(12.h),
-                  Text(
-                    "x",
-                    style: TextStyles.font14DarkBlueMedium,
-                  ),
-                ],
-              ).setHorizontalPadding(16.w);
-            },
-          ),
+        verticalSpace(8.h),
+        BlocBuilder<HomeCubit, HomeState>(
+          bloc: context.read<HomeCubit>(),
+          builder: (context, state) {
+            return state.whenOrNull(
+                  loading: () {
+                    return const CircularProgressIndicator();
+                  },
+                  success: (data) {
+                    specialities = data;
+                    debugPrint("Data length = ${specialities.length}");
+                    return SizedBox(
+                      height: 130.h,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 3,
+                        itemBuilder: (context, index) {
+                          return SpecialistWidget(
+                            title: specialities[index].name,
+                          );
+                        },
+                      ),
+                    );
+                  },
+                ) ??
+                const SizedBox(); // Add null check and return a default widget if state is null
+          },
         ),
       ],
     );
