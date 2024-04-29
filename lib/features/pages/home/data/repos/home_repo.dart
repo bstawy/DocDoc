5fa25bc2ecdc0e4fb2bed23c79d9cbbf05002f1e
bstawy <125200508@sha.edu.eg>
2024-04-29 23:48:33 +0300

    Update API endpoints and add new models for doctor speciality and all doctors

--- lib/features/pages/home/data/repos/home_repo.dart
+++ lib/features/pages/home/data/repos/home_repo.dart
@@ -5,6 +5,7 @@ import '../../../../../core/config/Constants/app_constants.dart';
 import '../../../../../core/networking/api_error/api_error_handler.dart';
 import '../../../../../core/networking/api_result/api_result.dart';
 import '../../../../../core/networking/api_service/api_service.dart';
+import '../models/doctor_model.dart';
 import '../models/doctor_speciality_model.dart';
 
 class HomeRepo {
@@ -13,27 +14,54 @@ class HomeRepo {
 
   HomeRepo(this._apiService, this._hiveManager);
 
-  Future<ApiResult<List<DoctorSpecialityModel>>> getHomeData() async {
+  Future<ApiResult<List<DoctorSpecialityModel>>>
+      getDoctorSpecialityData() async {
     try {
       List<DoctorSpecialityModel> cachedData = _hiveManager
-          .retrieveData<DoctorSpecialityModel>(HiveBoxKeys.homeData);
+          .retrieveData<DoctorSpecialityModel>(HiveBoxKeys.doctorSpeciality);
 
       if (cachedData.isNotEmpty && cachedData.length == 10) {
-        debugPrint("Home data retrieved from cache");
+        debugPrint("Specialist data retrieved from cache");
         return ApiResult.success(cachedData);
       }
 
-      final response = await _apiService.getHomeData();
+      final response = await _apiService.getDoctorSpecialityData();
 
       _hiveManager.cacheData<DoctorSpecialityModel>(
-        boxKey: HiveBoxKeys.homeData,
+        boxKey: HiveBoxKeys.doctorSpeciality,
         dataList: response.homeData,
       );
-      debugPrint("Home data retrieved from api");
+      debugPrint("Specialist data retrieved from api");
 
       return ApiResult.success(response.homeData);
     } catch (error) {
-      debugPrint("Error Home data retrieved");
+      debugPrint("Error Specialist data retrieved");
+
+      return ApiResult.failure(ErrorHandler.handle(error));
+    }
+  }
+
+  Future<ApiResult<List<DoctorModel>>> getAllDoctorsData() async {
+    try {
+      List<DoctorModel> cachedData =
+          _hiveManager.retrieveData<DoctorModel>(HiveBoxKeys.allDoctors);
+
+      if (cachedData.isNotEmpty) {
+        debugPrint("Doctors data retrieved from cache");
+        return ApiResult.success(cachedData);
+      }
+
+      final response = await _apiService.getAllDoctors();
+
+      _hiveManager.cacheData<DoctorModel>(
+        boxKey: HiveBoxKeys.allDoctors,
+        dataList: response.doctors,
+      );
+      debugPrint("Doctors data retrieved from api");
+
+      return ApiResult.success(response.doctors);
+    } catch (error) {
+      debugPrint("Error Doctors data retrieved");
 
       return ApiResult.failure(ErrorHandler.handle(error));
     }
