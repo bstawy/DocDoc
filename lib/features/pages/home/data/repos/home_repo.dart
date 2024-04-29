0fa87cf704d0f1e12e694de8355024c0d24a0551
bstawy <125200508@sha.edu.eg>
2024-04-29 22:02:07 +0300

    Update HomeRepo to cache and retrieve data using HiveManager

--- lib/features/pages/home/data/repos/home_repo.dart
+++ lib/features/pages/home/data/repos/home_repo.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/caching/hive_manager.dart';
+import '../../../../../core/config/Constants/app_constants.dart';
 import '../../../../../core/networking/api_error/api_error_handler.dart';
 import '../../../../../core/networking/api_result/api_result.dart';
 import '../../../../../core/networking/api_service/api_service.dart';
@@ -5,14 +9,32 @@ import '../models/home_response_model.dart';
 
 class HomeRepo {
   final ApiService _apiService;
+  final HiveManager _hiveManager;
 
-  HomeRepo(this._apiService);
+  HomeRepo(this._apiService, this._hiveManager);
 
   Future<ApiResult<List<HomeData>>> getHomeData() async {
     try {
+      List<HomeData> cachedData =
+          _hiveManager.retrieveData<HomeData>(HiveBoxKeys.homeData);
+
+      if (cachedData.isNotEmpty && cachedData.length == 10) {
+        debugPrint("Home data retrieved from cache");
+        return ApiResult.success(cachedData);
+      }
+
       final response = await _apiService.getHomeData();
+
+      _hiveManager.cacheData<HomeData>(
+        boxKey: HiveBoxKeys.homeData,
+        dataList: response.homeData,
+      );
+      debugPrint("Home data retrieved from api");
+
       return ApiResult.success(response.homeData);
     } catch (error) {
+      debugPrint("Error Home data retrieved");
+
       return ApiResult.failure(ErrorHandler.handle(error));
     }
   }
