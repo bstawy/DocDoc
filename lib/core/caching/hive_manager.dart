cac60c90f92b089aa6f4f23bb35a6a091c115ad7
bstawy <125200508@sha.edu.eg>
2024-04-29 21:59:05 +0300

    Add HiveManager class for managing Hive database operations

---
+++ lib/core/caching/hive_manager.dart
@@ -0,0 +1,51 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+import '../../features/pages/home/data/models/doctor_model.dart';
+import '../../features/pages/home/data/models/home_response_model.dart';
+import '../config/Constants/app_constants.dart';
+
+class HiveManager {
+  HiveManager._();
+
+  static HiveManager? _instance;
+
+  static HiveManager getInstance() {
+    _instance ??= HiveManager._();
+    return _instance!;
+  }
+
+  Future<void> init() async {
+    await Hive.initFlutter();
+    _registerAdapters();
+    await _openHiveBoxes();
+  }
+
+  void _registerAdapters() {
+    Hive.registerAdapter(HomeDataAdapter());
+    Hive.registerAdapter(DoctorModelAdapter());
+    Hive.registerAdapter(SpecializationModelAdapter());
+    Hive.registerAdapter(CityModelAdapter());
+    Hive.registerAdapter(GovernrateModelAdapter());
+  }
+
+  Future<void> _openHiveBoxes() async {
+    await Hive.openBox<HomeData>(HiveBoxKeys.homeData);
+  }
+
+  void cacheData<T>(
+      {required String boxKey, List<T>? dataList, T? dataItem}) async {
+    var box = Hive.box<T>(boxKey);
+
+    if (dataList != null) {
+      box.addAll(dataList);
+    } else if (dataItem != null) {
+      box.add(dataItem);
+    }
+  }
+
+  List<T> retrieveData<T>(String boxKey) {
+    var box = Hive.box<T>(boxKey);
+
+    return box.values.toList();
+  }
+}
