43ee512f0b3d7871467119bf4c1108badb775db8
bstawy <125200508@sha.edu.eg>
2024-05-01 20:19:41 +0300

    Refactor DocDocApp to remove isDevFlavor parameter and set debugShowCheckedModeBanner to false

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,28 +7,23 @@
 
 import 'package:docdoc/core/config/routing/app_router.dart';
 import 'package:docdoc/doc_doc_app.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(DocDocApp(
-      // TODO: Change this to false to test production flavor
-      isDevFlavor: true,
-      appRouter: AppRouter(),
-    ));
+    await tester.pumpWidget(DocDocApp(appRouter: AppRouter()));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // // Verify that our counter starts at 0.
+    // expect(find.text('0'), findsOneWidget);
+    // expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // // Tap the '+' icon and trigger a frame.
+    // await tester.tap(find.byIcon(Icons.add));
+    // await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // // Verify that our counter has incremented.
+    // expect(find.text('0'), findsNothing);
+    // expect(find.text('1'), findsOneWidget);
   });
 }
