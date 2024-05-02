cf353c029983d4a54918641ebcf71ebefef3e153
bstawy <125200508@sha.edu.eg>
2024-05-02 21:10:40 +0300

    Add DoctorsState, DoctorsCubit, and DoctorsRepo classes for managing doctors data in the home screen

---
+++ lib/features/pages/home/ui/pages/doctors/data/repos/doctors_repo.dart
@@ -0,0 +1,35 @@
+import '../../../../../../../../core/caching/hive_manager.dart';
+import '../../../../../../../../core/config/Constants/app_constants.dart';
+import '../../../../../../../../core/networking/api_error/api_error_handler.dart';
+import '../../../../../../../../core/networking/api_result/api_result.dart';
+import '../../../../../../../../core/networking/api_service/api_service.dart';
+import '../../../../../data/models/doctor_model.dart';
+
+class DoctorsRepo {
+  final ApiService _apiService;
+  final HiveManager _hiveManager;
+
+  DoctorsRepo(this._apiService, this._hiveManager);
+
+  Future<ApiResult<List<DoctorModel>>> getAllDoctors() async {
+    try {
+      List<DoctorModel> doctors =
+          _hiveManager.retrieveData<DoctorModel>(HiveBoxKeys.allDoctors);
+
+      if (doctors.isNotEmpty) {
+        return ApiResult.success(doctors);
+      }
+
+      final response = await _apiService.getAllDoctors();
+
+      _hiveManager.cacheData(
+        boxKey: HiveBoxKeys.allDoctors,
+        dataList: response.doctors,
+      );
+
+      return ApiResult.success(response.doctors);
+    } catch (error) {
+      return ApiResult.failure(ErrorHandler.handle(error));
+    }
+  }
+}
---
+++ lib/features/pages/home/ui/pages/doctors/logic/doctors_cubit.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../data/repos/doctors_repo.dart';
+import 'doctors_state.dart';
+
+class DoctorsCubit extends Cubit<DoctorsState> {
+  final DoctorsRepo _doctorsRepo;
+
+  DoctorsCubit(this._doctorsRepo) : super(const DoctorsState.initial());
+
+  Future<void> getAllDoctors() async {
+    emit(const DoctorsState.doctorsListLoading());
+
+    final result = await _doctorsRepo.getAllDoctors();
+
+    result.when(
+      success: (data) {
+        emit(DoctorsState.doctorsListSuccess(data));
+      },
+      failure: (error) {
+        emit(DoctorsState.failure(
+            error.apiErrorModel.message ?? "Error Occurred"));
+      },
+    );
+  }
+}
