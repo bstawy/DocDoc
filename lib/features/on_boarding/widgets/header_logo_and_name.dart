9a12653efc4f3a12547d6bbd96cf589315ce6237
bstawy <125200508@sha.edu.eg>
2024-05-04 23:04:55 +0300

    refactor: Update onboarding screen with Gap widget for consistent spacing

--- lib/features/on_boarding/widgets/header_logo_and_name.dart
+++ lib/features/on_boarding/widgets/header_logo_and_name.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:gap/gap.dart';
 
 import '../../../core/config/theme/texts/text_styles.dart';
 
@@ -13,7 +14,7 @@ class HeaderLogoAndName extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         SvgPicture.asset("assets/images/docdoc_logo.svg"),
-        SizedBox(width: 8.w),
+        Gap(8.w),
         Text(
           "Docdoc",
           style: TextStyles.font24BlueBold.copyWith(
