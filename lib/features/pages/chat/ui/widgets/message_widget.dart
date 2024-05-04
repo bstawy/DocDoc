206365ef0853fee90e63da801dfe7ee4646803fd
bstawy <125200508@sha.edu.eg>
2024-05-04 17:12:52 +0300

    feat: Add MessageWidget for displaying chat messages

---
+++ lib/features/pages/chat/ui/widgets/message_widget.dart
@@ -0,0 +1,80 @@
+import 'package:docdoc/core/config/theme/texts/text_styles.dart';
+import 'package:docdoc/core/helpers/extensions/extensions.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../../core/config/theme/colors/light_color_scheme.dart';
+
+class MessageWidget extends StatelessWidget {
+  const MessageWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CircleAvatar(
+              radius: 30.r,
+              backgroundImage:
+                  const AssetImage("assets/images/home_find_nearby_doctor.png"),
+            ).setHorizontalPadding(8.w),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Dr.Randy Wigham",
+                    style: TextStyles.font14DarkBlueMedium,
+                  ),
+                  Text(
+                    "General Doctor",
+                    style: TextStyles.font12GreyRegular,
+                  ),
+                  verticalSpace(8.h),
+                  Text(
+                    "Fine, I'll do a check. Does the patient have a history of certain diseases?",
+                    style: TextStyles.font12GreyRegular,
+                  ),
+                ],
+              ),
+            ),
+            horizontalSpace(16.w),
+            Column(
+              children: [
+                Text(
+                  "7:15 AM",
+                  style: TextStyles.font12GreyRegular,
+                ),
+                verticalSpace(32.h),
+                Container(
+                  width: 20.r,
+                  height: 20.r,
+                  decoration: BoxDecoration(
+                    color: ColorsManager.mainBlue,
+                    borderRadius: BorderRadius.circular(6.r),
+                  ),
+                  alignment: Alignment.center,
+                  child: Text(
+                    "2",
+                    style: TextStyles.font12BlueRegular.copyWith(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ).setHorizontalPadding(16.w),
+        verticalSpace(16.h),
+        const Divider(
+          color: ColorsManager.lightGrey,
+          thickness: 0.5,
+        ).setHorizontalPadding(16.w),
+        verticalSpace(16.h),
+      ],
+    );
+  }
+}
