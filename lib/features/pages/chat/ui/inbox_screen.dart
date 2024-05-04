4ff0f86694166689523b0bbaf24ed85e50d5ed40
bstawy <125200508@sha.edu.eg>
2024-05-04 17:12:20 +0300

    refactor: Update inbox screen UI to include search bar and message list

--- lib/features/pages/chat/ui/inbox_screen.dart
+++ lib/features/pages/chat/ui/inbox_screen.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/helpers/extensions/extensions.dart';
+import '../../home/ui/pages/doctors/ui/widgets/search_bar_widget.dart';
+import 'widgets/custom_app_bar_widget.dart';
+import 'widgets/message_widget.dart';
 
 class InboxScreen extends StatelessWidget {
   const InboxScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Inbox Screen',
-        style: TextStyle(fontSize: 24, color: Colors.black),
-      ),
+    return Column(
+      children: [
+        const CustomAppBar(),
+        Expanded(
+          child: ListView(
+            children: [
+              const SearchBarWidget(doctors: [])
+                  .setHorizontalAndVerticalPadding(16.w, 32.h),
+              Column(
+                children: List.generate(
+                  10,
+                  (index) => const MessageWidget(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
