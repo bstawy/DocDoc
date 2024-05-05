64b38302c97ed82a1046feab8e52389148d9d6a9
bstawy <125200508@sha.edu.eg>
2024-05-05 16:53:41 +0300

    feat: Add lazy loading for splash screen logo

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -47,9 +47,11 @@ class _SplashScreenState extends State<SplashScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Lottie.asset(
-          "assets/animation/docdoc_animated_splash_logo.json",
+      body: SafeArea(
+        child: Center(
+          child: Lottie.asset(
+            "assets/animation/docdoc_animated_splash_logo.json",
+          ),
         ),
       ),
     );
