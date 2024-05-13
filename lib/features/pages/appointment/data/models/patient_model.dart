34954bfbcbdb49f6ff3f2c411b3a18d125d5f1d5
bstawy <125200508@sha.edu.eg>
2024-05-13 14:35:20 +0300

    feat: Add HiveManager support for PatientModel
    This commit adds support for the `PatientModel` class in the `HiveManager` class. The `PatientModel` is registered as an adapter in the `HiveManager` to enable caching and retrieval of patient data. This enhancement improves the functionality of the application by allowing efficient storage and retrieval of patient data using Hive.

--- lib/features/pages/appointment/data/models/patient_model.dart
+++ lib/features/pages/appointment/data/models/patient_model.dart
@@ -1,13 +1,20 @@
+import 'package:hive/hive.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'patient_model.g.dart';
 
+@HiveType(typeId: 6)
 @JsonSerializable()
 class PatientModel {
+  @HiveField(0)
   final int id;
+  @HiveField(1)
   final String name;
+  @HiveField(2)
   final String email;
+  @HiveField(3)
   final String phone;
+  @HiveField(4)
   final String gender;
 
   PatientModel({
