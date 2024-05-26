a5e0bef513f58dffef03f0212429864c0faa3eb2
bastawy <125200508@sha.edu.eg>
2024-05-26 17:09:55 +0300

    feat: Add ProfileItem, AppointmentsAndRecordsButtons, and profile icons
    This commit adds the ProfileItem widget, which displays an icon and title for a specific profile item. It also adds the AppointmentsAndRecordsButtons widget, which displays two buttons for accessing the user's appointments and medical records. Additionally, it includes the profile payment, personal card, and report icons. These enhancements improve the functionality and user experience of the profile screen.

--- lib/features/pages/profile/ui/profile_screen.dart
+++ lib/features/pages/profile/ui/profile_screen.dart
@@ -1,14 +1,21 @@
-import 'package:docdoc/core/helpers/extensions/extensions.dart';
-import 'package:docdoc/core/widgets/custom_text_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
 
+import '../../../../core/caching/hive_manager.dart';
+import '../../../../core/caching/secure_storage_factory.dart';
+import '../../../../core/config/routing/routes.dart';
 import '../../../../core/config/theme/colors/colors_manager.dart';
 import '../../../../core/config/theme/texts/text_styles.dart';
+import '../../../../core/di/dependency_injection.dart';
+import '../../../../core/helpers/extensions/extensions.dart';
+import '../../../../core/networking/api_service/api_service.dart';
+import '../../../../core/widgets/custom_material_button.dart';
 import '../../../layout/logic/layout_cubit.dart';
 import '../../widgets/custom_app_bar_widget.dart';
+import 'widgets/appointments_and_records_buttons_widget.dart';
+import 'widgets/profile_item_widget.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -27,135 +34,117 @@ class _ProfileScreenState extends State<ProfileScreen> {
       },
       child: Container(
         color: ColorsManager.mainBlue,
-        child: Column(
-          children: [
-            CustomAppBar(
-              title: "Profile",
-              titleStyle: TextStyles.font18DarkBlueBold.copyWith(
-                color: Colors.white,
-              ),
-              leadingIconColor: Colors.white,
-              actionIcon: IconButton(
-                onPressed: () {},
-                padding: EdgeInsets.zero,
-                icon: const Icon(
-                  Icons.settings,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              CustomAppBar(
+                title: "Profile",
+                titleStyle: TextStyles.font18DarkBlueBold.copyWith(
                   color: Colors.white,
                 ),
+                leadingIconColor: Colors.white,
+                actionIcon: IconButton(
+                  onPressed: () {},
+                  padding: EdgeInsets.zero,
+                  icon: const Icon(
+                    Icons.settings,
+                    color: Colors.white,
+                  ),
+                ),
               ),
-            ),
-            Expanded(
-              child: Stack(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 92.h),
-                    padding: EdgeInsets.only(top: 92.w),
-                    width: 1.sw,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(24.r),
-                        topRight: Radius.circular(24.r),
+              SizedBox(
+                height: 1.sh - 92.h,
+                child: Stack(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 92.h),
+                      padding: EdgeInsets.only(top: 92.w),
+                      width: 1.sw,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(24.r),
+                          topRight: Radius.circular(24.r),
+                        ),
                       ),
                     ),
-                  ),
-                  Positioned(
-                    top: 40.h,
-                    left: 0,
-                    right: 0,
-                    child: Column(
-                      children: [
-                        CircleAvatar(
-                          radius: 70,
-                          child: Image.asset("assets/images/profile_pic.png"),
-                        ),
-                        Gap(35.h),
-                        Text(
-                          "Bastawy",
-                          style: TextStyles.font20DarkBlueSemiBold,
-                        ),
-                        Gap(8.h),
-                        Text(
-                          "b@b.com",
-                          style: TextStyles.font14GreyRegular,
-                        ),
-                        Gap(24.h),
-                        Container(
-                          height: 60.h,
-                          width: 1.sw,
-                          decoration: BoxDecoration(
-                            color: const Color(0xffF8F8F8),
-                            borderRadius: BorderRadius.circular(16.r),
+                    Positioned(
+                      top: 40.h,
+                      left: 0,
+                      right: 0,
+                      child: Column(
+                        children: [
+                          CircleAvatar(
+                            radius: 70,
+                            child: Image.asset("assets/images/profile_pic.png"),
                           ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Expanded(
-                                child: CustomTextButton(
-                                  onPressed: () {},
-                                  text: "My Appointments",
-                                  style:
-                                      TextStyles.font12DarkBlueMedium.copyWith(
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                              ),
-                              const VerticalDivider(
-                                color: ColorsManager.lighterGrey,
-                                thickness: 1,
-                              ).setVerticalPadding(8.h),
-                              Expanded(
-                                child: CustomTextButton(
-                                  onPressed: () {},
-                                  text: "Medical records",
-                                  style:
-                                      TextStyles.font12DarkBlueMedium.copyWith(
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                              ),
-                            ],
+                          Gap(24.h),
+                          Text(
+                            "Bastawy",
+                            style: TextStyles.font20DarkBlueSemiBold,
                           ),
-                        ),
-                      ],
-                    ).setHorizontalPadding(24.w),
-                  ),
-                ],
-              ),
-            )
-          ],
+                          Text(
+                            "b@b.com",
+                            style: TextStyles.font14LightGreyRegular,
+                          ),
+                          Gap(24.h),
+                          const AppointmentsAndRecordsButtons(),
+                          Gap(24.h),
+                          ProfileItem(
+                            title: "Personal Information",
+                            iconPath:
+                                "assets/icons/profile_personal_card_icon.svg",
+                            onTap: () {},
+                          ),
+                          ProfileItem(
+                            title: "My Test & Diagnostic",
+                            iconPath: "assets/icons/profile_report_icon.svg",
+                            onTap: () {},
+                          ),
+                          ProfileItem(
+                            title: "Payment",
+                            iconPath: "assets/icons/profile_payment_icon.svg",
+                            onTap: () {},
+                          ),
+                          CustomMaterialButton(
+                            onClicked: () async {
+                              final ApiService apiServices =
+                                  getIt<ApiService>();
+
+                              try {
+                                final response = await apiServices.logout();
+
+                                if (response.code == 200) {
+                                  HiveManager.getInstance().clearAllData();
+                                  SecureStorage.getInstance()
+                                      .delete(key: "mytoken");
+
+                                  if (context.mounted) {
+                                    context.pushNamedAndRemoveUntil(
+                                      Routes.onBoardingScreen,
+                                      predicate: ModalRoute.withName(
+                                          Routes.splashScreen),
+                                    );
+                                  }
+                                } else {
+                                  throw Exception(response.message);
+                                }
+                              } catch (e) {
+                                debugPrint("Error: $e");
+                              }
+                            },
+                            title: 'Logout',
+                          ).setHorizontalPadding(40.w),
+                        ],
+                      ).setHorizontalPadding(24.w),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
-/*
-Center(
-        child: CustomMaterialButton(
-          onClicked: () async {
-            final ApiService apiServices = getIt<ApiService>();
-
-            try {
-              final response = await apiServices.logout();
-
-              if (response.code == 200) {
-                HiveManager.getInstance().clearAllData();
-                SecureStorage.getInstance().delete(key: "mytoken");
-
-                if (context.mounted) {
-                  context.pushNamedAndRemoveUntil(
-                    Routes.onBoardingScreen,
-                    predicate: ModalRoute.withName(Routes.splashScreen),
-                  );
-                }
-              } else {
-                throw Exception(response.message);
-              }
-            } catch (e) {
-              debugPrint("Error: $e");
-            }
-          },
-          title: 'Logout',
-        ).setHorizontalPadding(40.w),
-      ),
-      */
